58e3435a00ccc0ae9740e2350e5de2671924e565
DeepaBai <ideepabai2000@gmail.com>
2023-08-11 22:23:45 +0530

    Code 1

---
+++ src/main/java/GitHub/GITHUB/OccOfChar.java
@@ -0,0 +1,28 @@
+package GitHub.GITHUB;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class OccOfChar {
+
+	public static void main(String[] args) {
+
+		String str = "Deepa";
+
+		Map<Character, Integer> mp = new LinkedHashMap();
+
+		char[] c = str.toCharArray();
+
+		for (Character ch : c) {
+			if (mp.containsKey(ch)) {
+				int val = mp.get(ch);
+				mp.put(ch, val + 1);
+			}
+
+			else {
+				mp.put(ch, 1);
+			}
+		}
+		System.out.println(mp);
+	}
+}
